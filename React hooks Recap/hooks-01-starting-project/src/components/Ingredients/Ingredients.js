c01e2984c74db3d14ff725344c1bfb3d4a5bbc2e
ADAPA VENKATA NAGENDRA BHARATH <74871715+bharth-stack@users.noreply.github.com>
2022-02-22 21:52:49 +0530

    ADD LIST

---
+++ React hooks Recap/hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect, useCallback } from "react";
+
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
+
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://react-http-cc433-default-rtdb.firebaseio.com/ingredients.json"
+    )
+      .then((response) => {
+        setIsLoading(false);
+        // return response.json();
+      })
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log("RENDERING INGREDIENTS", userIngredients);
+  }, [userIngredients]);
+
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
+    setUserIngredients(filteredIngredients);
+  }, []);
+
+  const addIngredientHandler = (ingredient) => {
+    fetch(
+      "https://react-http-cc433-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = (ingredientId) => {
+    setIsLoading(true);
+    fetch(
+      `https://react-http-cc433-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`,
+      {
+        method: "DELETE",
+      }
+    ).then((response) => {
+      setUserIngredients((prevIngredients) =>
+        prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+      );
+    });
+  };
+
+  return (
+    <div className="App">
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
+
+      <section>
+        <Search onLoadIngredients={filteredIngredientsHandler} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
+      </section>
+    </div>
+  );
+};
+
+export default Ingredients;
---
+++ React hooks Recap/hooks-01-starting-project/src/components/Ingredients/Search.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect, useRef } from "react";
+
+import Card from "../UI/Card";
+import "./Search.css";
+
+const Search = React.memo((props) => {
+  const { onLoadIngredients } = props;
+  const [enteredFilter, setEnteredFilter] = useState("");
+  const inputRef = useRef();
+  useEffect(() => {
+    if (enteredFilter === inputRef.current.value) {
+      const query =
+        enteredFilter.length === 0
+          ? ""
+          : `?orderBy="title"&equalTo="${enteredFilter}"`;
+      fetch(
+        "https://react-http-cc433-default-rtdb.firebaseio.com/ingredients.json" +
+          query
+      )
+        .then((response) => response.json())
+        .then((responseData) => {
+          const loadedIngredients = [];
+          for (const key in responseData) {
+            loadedIngredients.push({
+              id: key,
+              title: responseData[key].title,
+              amount: responseData[key].amount,
+            });
+          }
+          onLoadIngredients(loadedIngredients);
+        });
+    }
+  }, [enteredFilter, onLoadIngredients]);
+
+  return (
+    <section className="search">
+      <Card>
+        <div className="search-input">
+          <label>Filter by Title</label>
+          <input
+            ref={inputRef}
+            type="text"
+            value={enteredFilter}
+            onChange={(event) => setEnteredFilter(event.target.value)}
+          />
+        </div>
+      </Card>
+    </section>
+  );
+});
+
+export default Search;

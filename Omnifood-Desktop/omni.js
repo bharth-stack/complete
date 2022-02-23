76e619f2707f1e9010cd5c7c59d24ea68a8634c9
ADAPA VENKATA NAGENDRA BHARATH <74871715+bharth-stack@users.noreply.github.com>
2022-02-23 23:16:11 +0530

    Omni Food

---
+++ Omnifood-Desktop/omni.js
@@ -0,0 +1,18 @@
+const el = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(el);

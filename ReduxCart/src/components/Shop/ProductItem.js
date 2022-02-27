88e29c5f723dce1f4a8b610f95673ee57b50eaf6
ADAPA VENKATA NAGENDRA BHARATH <74871715+bharth-stack@users.noreply.github.com>
2022-02-27 18:25:02 +0530

    ReduxCart

---
+++ ReduxCart/src/components/Shop/ProductItem.js
@@ -0,0 +1,43 @@
+import { useDispatch } from "react-redux";
+
+import { cartActions } from "../../store/cart-slice";
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
+
+const ProductItem = (props) => {
+  const dispatch = useDispatch();
+
+  const { title, price, description, id } = props;
+
+  const addToCartHandler = () => {
+    fetch("https://react-http-cc433-default-rtdb.firebaseio.com/cart.json", {
+      method: "POST",
+      body: JSON.stringify(),
+    });
+
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
+
+  return (
+    <li className={classes.item}>
+      <Card>
+        <header>
+          <h3>{title}</h3>
+          <div className={classes.price}>${price.toFixed(2)}</div>
+        </header>
+        <p>{description}</p>
+        <div className={classes.actions}>
+          <button onClick={addToCartHandler}>Add to Cart</button>
+        </div>
+      </Card>
+    </li>
+  );
+};
+
+export default ProductItem;
